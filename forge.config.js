4664f8fd2ca0831112f092d8a144b4e9afd2daeb
dgoudie <34222208+dgoudie@users.noreply.github.com>
2020-08-13 20:09:12 -0500

    updated forge config

--- forge.config.js
+++ forge.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const fileUrl = require('file-url');
 
 module.exports = {
     packagerConfig: {
         appCopyright: `Copyright © ${new Date().getUTCFullYear()} dgoudie`,
         appBundleId: 'dev.goudie.project-opener',
+        icon: path.join(__dirname, 'src/assets', 'logo.ico'),
     },
     makers: [
         {
@@ -11,7 +13,9 @@ module.exports = {
             config: {
                 name: 'project_opener',
                 setupIcon: path.join(__dirname, 'src/assets', 'logo.ico'),
-                iconUrl: path.join(__dirname, 'src/assets', 'logo.ico'),
+                iconUrl: fileUrl(
+                    path.join(__dirname, 'src/assets', 'logo.ico')
+                ),
             },
         },
         {
