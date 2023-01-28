e15ab479066905622f87259756031d3c2fe21978
dgoudie <34222208+dgoudie@users.noreply.github.com>
2023-01-28 14:26:23 -0600

    fixed logo

--- forge.config.ts
+++ forge.config.ts
@@ -4,14 +4,24 @@ import { MakerRpm } from '@electron-forge/maker-rpm';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
+import fileUrl from 'file-url';
 import { mainConfig } from './webpack.main.config';
+import path from 'path';
 import { rendererConfig } from './webpack.renderer.config';
 
 const config: ForgeConfig = {
-  packagerConfig: {},
+  packagerConfig: {
+    appCopyright: `Copyright © ${new Date().getUTCFullYear()} dgoudie`,
+    appBundleId: 'dev.goudie.project-opener',
+    icon: path.join(__dirname, 'src/assets', 'logo.ico'),
+  },
   rebuildConfig: {},
   makers: [
-    new MakerSquirrel({}),
+    new MakerSquirrel({
+      name: 'project_opener',
+      setupIcon: path.join(__dirname, 'src/assets', 'logo.ico'),
+      iconUrl: fileUrl(path.join(__dirname, 'src/assets', 'logo.ico')),
+    }),
     new MakerZIP({}, ['darwin']),
     new MakerRpm({}),
     new MakerDeb({}),
