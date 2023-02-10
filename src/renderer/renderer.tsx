c8f49bdc3b7a7f822c27af8060392d71734a2261
dgoudie <34222208+dgoudie@users.noreply.github.com>
2023-02-10 07:40:59 -0700

    updated fonts

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,11 +1,18 @@
 import '@fontsource/roboto-slab';
+import '@fontsource/source-sans-pro';
 
-import { BaseStyles, ThemeProvider, ThemeProviderProps } from '@primer/react';
+import {
+  BaseStyles,
+  ThemeProvider,
+  ThemeProviderProps,
+  theme,
+} from '@primer/react';
 
 import App from './App';
 import { BRIDGE } from './bridge';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import deepmerge from 'deepmerge';
 import { ipcRenderer } from 'electron';
 import { settingsTable } from './indexed-db';
 
@@ -15,13 +22,19 @@ declare global {
   }
 }
 
+const customTheme = deepmerge(theme, {
+  fonts: {
+    normal: "'Source Sans Pro', sans-serif;",
+  },
+});
+
 Promise.all([
   settingsTable
     .get('THEME')
     .then(({ value }) => value as ThemeProviderProps['colorMode']),
 ]).then(([theme]) =>
   createRoot(document.getElementById('root')).render(
-    <ThemeProvider colorMode={theme}>
+    <ThemeProvider theme={customTheme} colorMode={theme}>
       <BaseStyles>
         <App />
       </BaseStyles>
